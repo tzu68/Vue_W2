067dcca010e544df324e9cb12599fe33c6355879
tzu <floya9733@gmail.com>
2024-08-14 07:05:57 +0800

    環境建立，初次API

---
+++ src/views/Week2View.vue
@@ -0,0 +1,17 @@
+<script setup>
+import axios from 'axios'
+
+login()
+async function login() {
+  try {
+    const response = await axios.get('https://randomuser.me/ap')
+    console.log(response, response.value)
+  } catch (error) {
+    console.log(error, error.response)
+  }
+}
+</script>
+
+<template>
+  <h1>Hello</h1>
+</template>
